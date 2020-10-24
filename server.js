d00e94c16313c9fdf33c5fd15e41aa8334a76abc
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2020-10-24 13:33:39 -0400

    Added enviroment prot

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = proc.env.port || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -89,6 +89,8 @@ app.post("/api/characters", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
+c
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
